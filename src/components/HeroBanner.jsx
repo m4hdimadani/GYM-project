92a8dae8518b74fb0d56d671294f87b601cc1334
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2025-08-04 10:32:01 +0330

    create project and create navbare and banner

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,40 @@
+import { Box, Stack, Typography, Button } from "@mui/material";
+import HerorBaneerImage from "../assets/images/banner.png";
+function HeroBanner() {
+  return (
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#ff2625" fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mt={3}
+      >
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+      <Typography fontSize="22px" lineHeight="35px" mb={3} fontFamily='cursive'>
+        Check out the most effective exercises
+      </Typography>
+      <Button variant="contained" color="error" href="#exercises" sx={{backgroundColor: '#ff2625', padding:'10px'}}>
+        Explore Exercises
+      </Button>
+      <Typography fontWeight={600} color="#ff2625" sx={{
+        opacity:0.1,
+        display:{lg: 'block' , xs: 'none'}
+      }} fontSize='230px' mt={3} >
+        Exercise
+      </Typography>
+      <img src={HerorBaneerImage} alt="banner" className="hero-banner-img" />
+    </Box>
+  );
+}
+
+export default HeroBanner;
